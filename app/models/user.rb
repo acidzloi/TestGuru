80f9f4ca57418447ef00db995c564e70f15e079a
root <root@cubic.local>
2025-06-27 13:07:57 +0300

    fix: удалил лишниие методы

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  # Найти все тесты пользователя по уровню сложности
+  def tests_user_started(level)
+    Test.joins("INNER JOIN test_passages ON tests.id = test_passages.test_id")
+        .where("test_passages.user_id = ? AND tests.level = ?", id, level)
+        .distinct
+  end
+end
