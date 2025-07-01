15850f4e5b85effd34af3643412319f6157d4c8f
root <root@cubic.local>
2025-07-01 12:21:09 +0300

    fix: исправил по результату review

--- db/migrate/20250629144114_create_results.rb
+++ db/migrate/20250629144114_create_results.rb
@@ -3,7 +3,6 @@ class CreateResults < ActiveRecord::Migration[6.1]
     create_table :results do |t|
       t.references :user, null: false, foreign_key: true
       t.references :test, null: false, foreign_key: true
-      t.string :status, null: false, default: 'not started'
 
       t.timestamps
     end
