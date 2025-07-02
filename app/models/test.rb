b888a507dd8d4e93a804c8e0936a650393c9fef5
root <root@cubic.local>
2025-07-02 16:44:44 +0300

    fiature: Обновил модели с использованием ассоциаций

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,8 +1,14 @@
 class Test < ApplicationRecord
+  belongs_to :category
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+
+  has_many :questions, dependent: :destroy
+  has_many :results, dependent: :destroy
+  has_many :users, through: :results, dependent: :destroy
 
   # Найти тесты по категории
   def self.titles_by_category(category_title)
-    joins("INNER JOIN categories ON categories.id = tests.category_id")
+    joins(:category)
       .where(category: { title: category_title })
       .order(title: :desc)
       .pluck(:title)
