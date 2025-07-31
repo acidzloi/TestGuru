76fe215e17102cef32f0b37c93e39e074f270c8e
root <root@cubic.local>
2025-07-31 16:43:47 +0300

    fiature: Ответ на часть аутентификация

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,14 @@
+require 'digest/sha1'
+
 class User < ApplicationRecord
   has_many :test_passages, dependent: :destroy
   has_many :tests, through: :test_passages, dependent: :destroy
   has_many :author_for_tests, class_name: 'Test', foreign_key: :author_id, dependent: :destroy
 
+  has_secure_password
+  
   validates :name, presence: true
+  validates :email, presence: true, format: URI::MailTo::EMAIL_REGEXP, uniqueness: { case_sensitive: false }
 
   def tests_user_started(level)
     tests.where(tests: { level: level })
