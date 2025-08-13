04dc1cae75ff078e7c4a7d21b4bb5ba7d9101b18
root <root@cubic.local>
2025-08-14 00:14:32 +0300

    fix: исправил по результату review на часть JavaScript

--- app/javascript/utilities/password_confirm.js
+++ app/javascript/utilities/password_confirm.js
@@ -1,29 +1,17 @@
 document.addEventListener('turbolinks:load', function() {
-  var password = document.getElementById('user_password')
-  var password_confirmation = document.getElementById('user_password_confirmation')
-
-  if (password && password_confirmation) {
-    password.addEventListener('input', validation)
-    password_confirmation.addEventListener('input', validation)
-  }
-})
-
-function validation() {
-  var password_value = document.getElementById("user_password").value
-  var password_confirmation_value = document.getElementById("user_password_confirmation").value
+  if (document.getElementById('user_password') && document.getElementById('user_password_confirmation')) {
+    let password = new ValidationUtility(document.getElementById('user_password'))
+    let password_confirmation = new ValidationUtility(document.getElementById('user_password_confirmation'))
   
-  if (password_confirmation_value.length == 0) {
-    this.classList.remove('bg-danger')
-    this.classList.remove('bg-success')
-    this.classList.add('bg-light')
-    return;
-  }
+    password.html_object.oninput = function() {
+      password.validation(this.value, password_confirmation.value)
+      password_confirmation.validation(password_confirmation.value, password.value)
+    }
 
-  if (password_value != password_confirmation_value) {
-    this.classList.remove('bg-success')
-    this.classList.add('bg-danger')
-  } else {
-    this.classList.remove('bg-danger')
-    this.classList.add('bg-success')
+    password_confirmation.html_object.oninput = function() {
+      password_confirmation.validation(this.value, password.value)
+      password.validation(password.value, password_confirmation.value)
+    }
+   }
   }
-}
+)
--- app/javascript/utilities/sorting.js
+++ app/javascript/utilities/sorting.js
@@ -1,43 +1,9 @@
 document.addEventListener('turbolinks:load', function() {
-  var controls = document.querySelectorAll('.sort-by-title')
-
-  controls.forEach(control => {
-    control.addEventListener('click', sortRowsByTitle)
-  })
+  let control = document.querySelector('.sort-by-title')
+  if (control) { control.addEventListener('click', sortTable) }
 })
 
-function sortRowsByTitle() {
-  var table = document.querySelector('table')
-  var tbody = table.querySelector('tbody')
-  var rows = Array.from(tbody.querySelectorAll('tr'))
-
-  if (this.querySelector('.octicon-arrow-up').classList.contains('hide')) {
-    rows.sort(compareRowsAsc)
-    this.querySelector('.octicon-arrow-up').classList.remove('hide')
-    this.querySelector('.octicon-arrow-down').classList.add('hide')
-  } else {
-    rows.sort(compareRowsDesc)
-    this.querySelector('.octicon-arrow-down').classList.remove('hide')
-    this.querySelector('.octicon-arrow-up').classList.add('hide')
-  }
-
-  rows.forEach(row => tbody.appendChild(row))
-}
-
-function compareRowsAsc(row1, row2) {
-  var testTitle1 = row1.querySelector('td').textContent
-  var testTitle2 = row2.querySelector('td').textContent
-
-  if (testTitle1 < testTitle2) return -1
-  if (testTitle1 > testTitle2) return 1
-  return 0
-}
-
-function compareRowsDesc(row1, row2) {
-  var testTitle1 = row1.querySelector('td').textContent
-  var testTitle2 = row2.querySelector('td').textContent
-
-  if (testTitle1 < testTitle2) return 1
-  if (testTitle1 > testTitle2) return -1
-  return 0
+function sortTable() {
+  let table = new SortTable(document.querySelector('table'))
+  table.sortTableByTitle()
 }
